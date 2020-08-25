e1501188e22abb0618936f5c5e5e6ecaba9d6b38
小天 <cloudin@foxmail.com>
2020-08-25 20:08:46 +0800

    移除视图名称开头的斜杠

--- springframework-velocity/src/main/java/org/springframework/ui/velocity/SpringResourceLoader.java
+++ springframework-velocity/src/main/java/org/springframework/ui/velocity/SpringResourceLoader.java
@@ -52,79 +52,85 @@ import org.springframework.util.StringUtils;
  */
 public class SpringResourceLoader extends ResourceLoader {
 
-	public static final String NAME = "spring";
+    public static final String NAME = "spring";
 
-	public static final String SPRING_RESOURCE_LOADER_CLASS = "spring.resource.loader.class";
+    public static final String SPRING_RESOURCE_LOADER_CLASS = "spring.resource.loader.class";
 
-	public static final String SPRING_RESOURCE_LOADER_CACHE = "spring.resource.loader.cache";
+    public static final String SPRING_RESOURCE_LOADER_CACHE = "spring.resource.loader.cache";
 
-	public static final String SPRING_RESOURCE_LOADER = "spring.resource.loader";
+    public static final String SPRING_RESOURCE_LOADER = "spring.resource.loader";
 
-	public static final String SPRING_RESOURCE_LOADER_PATH = "spring.resource.loader.path";
-
-
-	protected final Log logger = LogFactory.getLog(getClass());
-
-	private org.springframework.core.io.ResourceLoader resourceLoader;
-
-	private String[] resourceLoaderPaths;
+    public static final String SPRING_RESOURCE_LOADER_PATH = "spring.resource.loader.path";
 
 
-	@Override
-	public void init(ExtProperties extProperties) {
-		this.resourceLoader = (org.springframework.core.io.ResourceLoader)
-				this.rsvc.getApplicationAttribute(SPRING_RESOURCE_LOADER);
-		String resourceLoaderPath = (String) this.rsvc.getApplicationAttribute(SPRING_RESOURCE_LOADER_PATH);
-		if (this.resourceLoader == null) {
-			throw new IllegalArgumentException(
-					"'resourceLoader' application attribute must be present for SpringResourceLoader");
-		}
-		if (resourceLoaderPath == null) {
-			throw new IllegalArgumentException(
-					"'resourceLoaderPath' application attribute must be present for SpringResourceLoader");
-		}
-		this.resourceLoaderPaths = StringUtils.commaDelimitedListToStringArray(resourceLoaderPath);
-		for (int i = 0; i < this.resourceLoaderPaths.length; i++) {
-			String path = this.resourceLoaderPaths[i];
-			if (!path.endsWith("/")) {
-				this.resourceLoaderPaths[i] = path + "/";
-			}
-		}
-		if (logger.isInfoEnabled()) {
-			logger.info("SpringResourceLoader for Velocity: using resource loader [" + this.resourceLoader +
-					"] and resource loader paths " + Arrays.asList(this.resourceLoaderPaths));
-		}
-	}
+    protected final Log logger = LogFactory.getLog(getClass());
 
-	@Override
-	public Reader getResourceReader(String source, String encoding) throws ResourceNotFoundException {
-		if (logger.isDebugEnabled()) {
-			logger.debug("Looking for Velocity resource with name [" + source + "]");
-		}
-		for (String resourceLoaderPath : this.resourceLoaderPaths) {
-			org.springframework.core.io.Resource resource =
-					this.resourceLoader.getResource(resourceLoaderPath + source);
-			try {
-				return buildReader(resource.getInputStream(), encoding);
+    private org.springframework.core.io.ResourceLoader resourceLoader;
+
+    private String[] resourceLoaderPaths;
+
+
+    @Override
+    public void init(ExtProperties extProperties) {
+        this.resourceLoader = (org.springframework.core.io.ResourceLoader)
+                this.rsvc.getApplicationAttribute(SPRING_RESOURCE_LOADER);
+        String resourceLoaderPath = (String) this.rsvc.getApplicationAttribute(SPRING_RESOURCE_LOADER_PATH);
+        if (this.resourceLoader == null) {
+            throw new IllegalArgumentException(
+                    "'resourceLoader' application attribute must be present for SpringResourceLoader");
+        }
+        if (resourceLoaderPath == null) {
+            throw new IllegalArgumentException(
+                    "'resourceLoaderPath' application attribute must be present for SpringResourceLoader");
+        }
+        this.resourceLoaderPaths = StringUtils.commaDelimitedListToStringArray(resourceLoaderPath);
+        for (int i = 0; i < this.resourceLoaderPaths.length; i++) {
+            String path = this.resourceLoaderPaths[i];
+            if (!path.endsWith("/")) {
+                this.resourceLoaderPaths[i] = path + "/";
+            }
+        }
+        if (logger.isInfoEnabled()) {
+            logger.info("SpringResourceLoader for Velocity: using resource loader [" + this.resourceLoader +
+                    "] and resource loader paths " + Arrays.asList(this.resourceLoaderPaths));
+        }
+    }
+
+    @Override
+    public Reader getResourceReader(String source, String encoding) throws ResourceNotFoundException {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Looking for Velocity resource with name [" + source + "]");
+        }
+
+        // 移除开头的斜杠
+        while (source.startsWith("/")) {
+            source = source.substring(1);
+        }
+
+        for (String resourceLoaderPath : this.resourceLoaderPaths) {
+            org.springframework.core.io.Resource resource =
+                    this.resourceLoader.getResource(resourceLoaderPath + source);
+            try {
+                return buildReader(resource.getInputStream(), encoding);
 			}
 			catch (IOException ex) {
-				if (logger.isDebugEnabled()) {
-					logger.debug("Could not find Velocity resource: " + resource);
-				}
-			}
-		}
-		throw new ResourceNotFoundException(
-				"Could not find resource [" + source + "] in Spring resource loader path");
-	}
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Could not find Velocity resource: " + resource);
+                }
+            }
+        }
+        throw new ResourceNotFoundException(
+                "Could not find resource [" + source + "] in Spring resource loader path");
+    }
 
-	@Override
-	public boolean isSourceModified(Resource resource) {
-		return false;
-	}
+    @Override
+    public boolean isSourceModified(Resource resource) {
+        return false;
+    }
 
-	@Override
-	public long getLastModified(Resource resource) {
-		return 0;
-	}
+    @Override
+    public long getLastModified(Resource resource) {
+        return 0;
+    }
 
 }
